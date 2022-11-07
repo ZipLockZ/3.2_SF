84016ad1140d5c0601cdab883ce0eb7fe08ba850
ivan <gervadin.nomer1@gmail.com>
2022-11-07 18:34:56 +0300

    Использовали изученное в модуле 3.7

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,12 +2,23 @@
 {
     public static void Main(string[] args)
     {
+        Console.Write("Enter your name: ");
+        var name = Console.ReadLine();
 
-        string myName;
-        myName = "Jane";
+        Console.Write("Enter your age: ");
+        var age = Console.ReadLine();
+        Console.WriteLine("Your name is {0} and age is {1} ", name, age);
 
-        Console.WriteLine(myName);
+        Console.Write("What is your favorite day of week? ");
 
+        var day = (DayOfWeek)int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Your favorite day is {0}", day);
+
+        Console.Write("Write your birthdate: ");
+        var birthdate = Console.ReadLine();
+
+        Console.WriteLine("Your birthdate is {0}", birthdate);
         Console.ReadKey();
     }
 }
