c2f4547228f1c6e57ea4439cd853f3407755c66a
ivan <gervadin.nomer1@gmail.com>
2022-11-14 13:53:29 +0300

    4.1.17 Цвета консоли

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,23 +2,31 @@
 {
     public static void Main(string[] args)
     {
-        Console.Write("Enter your name: ");
-        var name = Console.ReadLine();
+        Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
 
-        Console.Write("Enter your age: ");
-        var age = Console.ReadLine();
-        Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+        var color = Console.ReadLine();
 
-        Console.Write("What is your favorite day of week? ");
+        if (color == "red")
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Black;
 
-        var day = (DayOfWeek)int.Parse(Console.ReadLine());
+            Console.WriteLine("Your color is red!");
+        }
 
-        Console.WriteLine("Your favorite day is {0}", day);
+        else if (color == "green")
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
 
-        Console.Write("Write your birthdate: ");
-        var birthdate = Console.ReadLine();
+            Console.WriteLine("Your color is green!");
+        }
+        else
+        {
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.Black;
 
-        Console.WriteLine("Your birthdate is {0}", birthdate);
-        Console.ReadKey();
+            Console.WriteLine("Your color is cyan!");
+        }
     }
 }
